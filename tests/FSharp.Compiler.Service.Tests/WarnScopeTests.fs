b18b3d6a5baee2f89beeadde3dd0ffbf4dd27a49
Martin521 <29605222+Martin521@users.noreply.github.com>
2024-09-22 13:25:44 +0000

    test cleanup

--- tests/FSharp.Compiler.Service.Tests/WarnScopeTests.fs
+++ tests/FSharp.Compiler.Service.Tests/WarnScopeTests.fs
@@ -1,10 +1,9 @@
 module FSharp.Compiler.Service.Tests.WarnScopeTests
 
 open Xunit
-open FsUnit
 open FSharp.Compiler.Service.Tests.Common
 
-let sourceForWarnScopesInScript =
+let sourceForWarnScopes =
     """
 module A
 match None with None -> ()
@@ -17,16 +16,9 @@ match None with None -> ()
     """
 
 [<Fact>]
-let WarnScopesInScript () =
-    let file = "WarnScopesInScript.fsx"
-    let parseResult, typeCheckResults = parseAndCheckScript(file, sourceForWarnScopesInScript)
-    printfn "parse errors:"
-    for diag in parseResult.Diagnostics do
-        printfn $"{diag.Range}: {diag.ErrorNumber}: {diag.Message}"
-    printfn "type check errors:"
-    for diag in typeCheckResults.Diagnostics do
-        printfn $"{diag.Range}: {diag.ErrorNumber}: {diag.Message}"
-    printfn "typeCheckResults.Diagnostics.Length = %d" typeCheckResults.Diagnostics.Length
+let WarnScopesWorkAsExpected () =
+    let file = "WarnScopesInScript.fs"
+    let parseResult, typeCheckResults = parseAndCheckScript(file, sourceForWarnScopes)
     Assert.Equal(parseResult.Diagnostics.Length, 0)
     Assert.Equal(typeCheckResults.Diagnostics.Length, 2)
     Assert.Equal(typeCheckResults.Diagnostics[0].ErrorNumber, 25)
